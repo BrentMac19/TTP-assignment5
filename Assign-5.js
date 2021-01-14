671a6b1fb10596ad5293508d16db77675e05c97e
BrentMac19 <Brentmaccallum1@yahoo.com>
2021-01-14 00:44:33 -0500

    Updated Color Functionality

--- Assign-5.js
+++ Assign-5.js
@@ -1,89 +1,135 @@
+let numRows = 1;
+let numCols = 1;
 
-let grid= document.getElementById("grid-con");
-let grid_style=document.getElementsByClassName("grid-item");
-let form=document.getElementById("form");
-let demo=document.getElementById("demo");  
-let r=3;
-let col=3;
+let cellColored = false;
+let dragColoring = false;
 
-//Submit form that prevents the grid from Resetting to orginal grid
-form.addEventListener("submit",function(event){
-    console.log(clear);
-    event.preventDefault();
-    console.log(event);
-    createGrid(r,col);
-})
-createGrid(r,col);
+//Adding Row
+function addRow(){
+    let grid = document.getElementById('grid');
+    let newRow = document.createElement("tr");
 
-
-function createGrid(r,col) {
-    grid.innerHTML='';
-    document.documentElement.style.setProperty('--col',col);
-    document.documentElement.style.setProperty('--row',r);
-    for(let i=0;i<r*col;i++){
-        grid.innerHTML+="<div class='grid-item'></div>";
+    for(let i = 0; i < numCols; i++) {
+        let cell = document.createElement("td");
+        newRow.appendChild(cell);
+        changeColor(cell);
     }
+    grid.appendChild(newRow);
+    numRows++;
+}
 
+//Adding Column
+function addCol(){
+    let rows = document.querySelectorAll("tr");
+    let rowIndex = 0;
+
+    for(let i = 0; i < numRows; i++) {
+        let cell = document.createElement("td");
+        changeColor(cell);
+        rows[rowIndex].appendChild(cell);
+        rowIndex++;
+    }
+    numCols++;
+}
+
+//Removing Row
+function removeRow(){
+    let grid = document.getElementById('grid');
+    grid.deleteRow(numRows-1);
+    numRows--;
+}
+
+//Removing Column
+function removeCol(){
+    let rows = document.querySelectorAll("tr");
+    let rowIndex = 0;
+    for(let i = 0; i < numRows; i++) {
+        rows[rowIndex].removeChild(rows[rowIndex].lastChild);
+        rowIndex++;
+    }
+    numCols--;
+}
+
+
+//setting Drop List color to a variable
+let chosenColor = 'pink';
+const chooseColor = (color) => {
+  chosenColor = color;
 };
-let row_adder=document.getElementById("add-row");
-row_adder.addEventListener("click",function(event){
-    event.preventDefault();
-    r+=1;
-    createGrid(r,col);
 
-})
-let col_adder=document.getElementById("add-columns");
-col_adder.addEventListener("click",function(event){
-    event.preventDefault();
-    col+=1;
-    createGrid(r,col);
-})
-let row_sub=document.getElementById("remove-row");
-row_sub.addEventListener("click",function(event){
-    event.preventDefault();
-    r-=1;
-    createGrid(r,col);
-})
-let col_sub=document.getElementById("remove-columns");
-col_sub.addEventListener("click",function(event){
-    event.preventDefault();
-    col-=1;
-    createGrid(r,col);
-})
+function changeColor(cell) {
+    cell.classList.add('isNotColored');
+    //Changing Color on a click
+    cell.addEventListener('click', colorChanged);
+
+    //test if user is holding mouse button down
+    cell.addEventListener('mousedown', (e) => {
+        cellColored = true;
+  });
+  //used to tell if the user is not holding the mouse button
+  cell.addEventListener('mouseup', (e) => {
+    if (cellColored) {
+    cellColored = false;
+    }
+  });
+  //used to color the grid while moving the mouse
+  cell.addEventListener('mousemove', (e) => {
+    if (cellColored) {
+        cell.style.backgroundColor = chosenColor;
+        cell.classList.remove('isNotColored');
+    }
+  });
+}
+
+function colorChanged() {
+  this.style.backgroundColor = chosenColor;
+  this.classList.remove('isNotColored');
+}
+
+//Filling all blank buttons with selected color
+function fillAllBlank() {
+    let cells = document.getElementsByTagName('td');
+    let cellsList = [...cells];
+
+    const uncoloredCells = cellsList.filter(cell => {
+        return cell.classList.contains('isNotColored');
+    });
+
+    uncoloredCells.forEach(cell => {
+        cell.style.backgroundColor = chosenColor;
+        cell.classList.remove('isNotColored');
+    })
+}
+
+//Changing the color of all the cells to selected color
+function fillAll(){
+    let cells = document.getElementsByTagName('td');
+    let cellsList = [...cells];
+
+    cellsList.forEach(cell => {
+        cell.style.backgroundColor = chosenColor;
+        cell.classList.remove('isNotColored');
+    })
+}
+
+//Clearing all the Cells color
+function clearAll() {
+    let cells = document.getElementsByTagName('td');
+    let cellsList = [...cells];
+
+    cellsList.forEach(cell => {
+        cell.style.backgroundColor = 'white';
+        cell.classList.add('isNotColored');
+    })
+}
 
 
+//any starting cells
+let cells = document.getElementsByTagName('td');
+let cellList = [...cells];
 
+for (let i=0; i < cellList.length; i++) {
+    const cell = cellList[i];
+    changeColor(cell);
+}
 
-
-
-// // function that clears the grid
-// function clearGrid(){
-//     $(".grid-item").remove();
-// };  
-
-// // function that prompts the user to select the number of boxes in a new grid
-// // the function then also creates that new grid
-// function refreshGrid(){
-//     var z = prompt("How many boxes per side?");
-//     clearGrid();
-//     createGrid(z);
-// };
-
-// // create a 16x16 grid when the page loads
-// // creates a hover effect that changes the color of a square to black when the mouse passes over it, leaving a (pixel) trail through the grid
-// // allows the click of a button to prompt the user to create a new grid
-// $(document).ready(function() {
-//     createGrid(16);
-
-//     $(".grid-item").mouseover(function() {
-//         $(this).css("background-color", "black");
-//         });
-
-//     $(".grid-container").click(function() {
-//         refreshGrid();
-
-//         $(".grid-item").mouseover(function() {
-//         $(this).css("background-color", "black");
-//         });
-//     });
-// });
